84cc96233ae43b9ea1e499de2c6875d8cd301047
cairofelipe <cairo.felipe@paraiba.email>
2022-01-19 18:02:52 -0300

    painel clean

--- admin/components/header.php
+++ admin/components/header.php
@@ -4,7 +4,6 @@
 <div class="d-flex align-items-center justify-content-between">
   <a href="index.html" class="logo d-flex align-items-center">
     <img src="../assets/img/logo-mit.png" alt="">
-    <span class="d-none d-lg-block">Admin</span>
   </a>
   <i class="bi bi-list toggle-sidebar-btn"></i>
 </div><!-- End Logo -->
