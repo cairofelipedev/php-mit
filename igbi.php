f47f813a49ffd6071bd50c4e4071084d2a016b59
cairofelipe <cairo.felipe@paraiba.email>
2022-01-25 16:46:07 -0300

    start igbi

---
+++ igbi.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Igbi Cash Back</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="./assets/img/igbi.jpg" rel="icon">
+  <link href="./assets/img/igbi.jpg" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/igbi.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Techie - v4.7.0
+  * Template URL: https://bootstrapmade.com/techie-free-skin-bootstrap-3/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center justify-content-between">
+      <a href="index.html" class="logo"><img src="assets/img/igbi-branca.png" alt="" class="img-fluid"></a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">Igbi Cashback</a></li>
+          <li><a class="nav-link scrollto" href="#contact">Contato</a></li>
+          <li><a class="getstarted scrollto" href="#about">Baixe agora</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container-fluid" data-aos="fade-up">
+      <div class="row justify-content-center">
+        <div class="col-xl-5 col-lg-6 pt-3 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+          <h1>OFEREÇA CASHBACK</h1>
+          <h2>E AUMENTE O FATURAMENTO DE SUA EMPRESA</h2>
+          <div><a href="#about" class="btn-get-started scrollto">Saiba mais</a></div>
+        </div>
+        <div class="col-xl-4 col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="150">
+          <img src="assets/img/celapp.png" class="img-fluid animated" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-lg-6 order-1 order-lg-2" data-aos="zoom-in" data-aos-delay="150">
+            <img src="assets/img/cashimg.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right">
+            <h3>UMA PLATAFORMA COMPLETA E FOCADA NO CRESCIMENTO DE NEGÓCIOS LOCAIS.</h3>
+            <p class="fst">
+              É UM PROGRAMA DE RECOMPENSAS FORA DA CURVA
+            </p>
+            <ul>
+              <li><i class="bi bi-check-circle"></i>EXPANSÃO DA SUA MARCA ATRAVÉS DE UM MARKETPLACE INTEGRADO</li>
+              <li><i class="bi bi-check-circle"></i>ACESSO A POTENCIAIS CLIENTES NOVOS TODOS OS DIAS</li>
+              <li><i class="bi bi-check-circle"></i>SUA EMPRESA SAI À FRENTE DA CONCORRÊNCIA</li>
+              <li><i class="bi bi-check-circle"></i>EXCLUSIVIDADE NO SEU NICHO</li>
+              <li><i class="bi bi-check-circle"></i>FIDELIZAÇÃO DE CLIENTES ATRAVÉS DO CASHBACK</li>
+              <li><i class="bi bi-check-circle"></i>EXPERIÊNCIA DE COMPRA DIFERENCIADA</li>
+              <li><i class="bi bi-check-circle"></i>AUMENTO NAS VENDAS E LUCRO DA SUA EMPRESA.</li>
+              <li><i class="bi bi-check-circle"></i>PLATAFORMA SIMPLES E INTUITIVA</li>
+              <li><i class="bi bi-check-circle"></i>TREINAMENTO EM VENDAS, FINANÇAS, MARKETING E IMPLANTAÇÃO DO SISTEMA</li>
+              <li><i class="bi bi-check-circle"></i>RESGATAR CLIENTES ANTIGOS DA BASE</li>
+            </ul>
+            <a href="#" class="read-more">Baixe agora <i class="bi bi-long-arrow-right"></i></a>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Frequently Asked Questions Section ======= -->
+    <section id="faq" class="faq">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Perguntas frequentes</h2>
+        </div>
+
+        <div class="faq-list">
+          <ul>
+            <li data-aos="fade-up" data-aos="fade-up" data-aos-delay="100">
+              <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-1"><i class="bi bi-question-circle"></i> O que é Cashback <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list">
+                <p>
+                  É dinheiro de volta. Simples assim. No Inter você compra, investe ou usa seu cartão e recebe parte do dinheiro de volta, direto na sua conta, para usar como quiser.
+                </p>
+              </div>
+            </li>
+
+            <li data-aos="fade-up" data-aos="fade-up" data-aos-delay="100">
+              <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-1"><i class="bi bi-question-circle"></i> O que é Cashback <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list">
+                <p>
+                  É dinheiro de volta. Simples assim. No Inter você compra, investe ou usa seu cartão e recebe parte do dinheiro de volta, direto na sua conta, para usar como quiser.
+                </p>
+              </div>
+            </li>
+            <li data-aos="fade-up" data-aos="fade-up" data-aos-delay="100">
+              <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-1"><i class="bi bi-question-circle"></i> O que é Cashback <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list">
+                <p>
+                  É dinheiro de volta. Simples assim. No Inter você compra, investe ou usa seu cartão e recebe parte do dinheiro de volta, direto na sua conta, para usar como quiser.
+                </p>
+              </div>
+            </li>
+            <li data-aos="fade-up" data-aos="fade-up" data-aos-delay="100">
+              <a data-bs-toggle="collapse" class="collapse" data-bs-target="#faq-list-1"><i class="bi bi-question-circle"></i> O que é Cashback <i class="bx bx-chevron-down icon-show"></i><i class="bx bx-chevron-up icon-close"></i></a>
+              <div id="faq-list-1" class="collapse show" data-bs-parent=".faq-list">
+                <p>
+                  É dinheiro de volta. Simples assim. No Inter você compra, investe ou usa seu cartão e recebe parte do dinheiro de volta, direto na sua conta, para usar como quiser.
+                </p>
+              </div>
+            </li>
+
+          </ul>
+        </div>
+
+      </div>
+    </section><!-- End Frequently Asked Questions Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>JUNTOS CRESCEMOS MAIS!</h2>
+          <p>Preencha o formulário abaixo e receba atendimento especializado</p>
+        </div>
+
+        <div class="row justify-content-center">
+
+          <div class="col-lg-6">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+              <div class="row">
+                <div class="col-md-6 form-group">
+                  <input type="text" name="name" class="form-control" id="name" placeholder="Nome" required>
+                </div>
+                <div class="col-md-6 form-group mt-3 mt-md-0">
+                  <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
+                </div>
+              </div>
+              <div class="form-group mt-3">
+                <input type="text" class="form-control" name="subject" id="subject" placeholder="Assunto" required>
+              </div>
+              <div class="form-group mt-3">
+                <textarea class="form-control" name="message" rows="5" placeholder="Mensagem" required></textarea>
+              </div>
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your message has been sent. Thank you!</div>
+              </div>
+              <div class="text-center"><button type="submit">Enviar</button></div>
+            </form>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+          <a href="index.html"><img src="assets/img/igbi-branca.png" alt="" width="100px"></a>
+            <p>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-5 col-md-6 footer-links">
+            <h4>IGBI.COM</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Igbi Cashback</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Perguntas frequentes</a></li>
+            </ul>
+          </div>
+<!-- 
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+            </ul>
+          </div> -->
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Newsletter</h4>
+            <p>Assine nossa Newsletter e receba conteúdo exclusivo de cashback</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Assine">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <!-- <div class="copyright-wrap d-md-flex py-4">
+        <div class="me-md-auto text-center text-md-start">
+          <div class="copyright">
+            &copy; Copyright <strong><span>Techie</span></strong>. All Rights Reserved
+          </div>
+          <div class="credits">
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          </div>
+        </div>
+        <div class="social-links text-center text-md-right pt-3 pt-md-0">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div> -->
+
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/igbi.js"></script>
+
+</body>
+
+</html>
